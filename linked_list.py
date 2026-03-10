bf5c1cd0749c4fc52ae48205fe2b340d0ce329d4
camarottoMD <matheus.domingues2@estudante.ifms.edu.br>
2026-03-10 19:22:34 -0300

    implementação da classe Node e LinkedList com métodos básicos

---
+++ linked_list.py
@@ -0,0 +1,49 @@
+
+#Cada nó da lista precisa tem seu valor e uma referencia para o próximo valor
+
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self._size = 0
+
+    """
+        Inserir elemento no ínicio da lista
+    """
+    def insert_beginning(self, value):
+
+
+    """
+        Inserir elemento no final da lista
+    """
+    def insert_end(value):
+
+    """
+        remover um elemento da lista
+    """
+    def remove(value):
+
+    """
+        buscar um elemento da lista
+    """
+    def search(value):
+
+    """
+        imprimir os elementos na lista
+    """
+    def print_list():
+
+    """
+        retornar o tamanho da lista
+    """
+    def size():
+
+    """
+        verificar se a lista está vazia
+    """
+    def is_empty():
