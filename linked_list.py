2d47c292b76c9974d3a64f7cf2634299853ab0b4
camarottoMD <matheus.domingues2@estudante.ifms.edu.br>
2026-03-11 16:54:48 -0300

    desenvolvendo lógica para insert_beginning

--- linked_list.py
+++ linked_list.py
@@ -10,40 +10,50 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-        self._size = 0
+        self._size = 0 # atributo privado
 
     """
         Inserir elemento no ínicio da lista
     """
     def insert_beginning(self, value):
-
+        if self.head:
+            node = Node(value)
+            aux = node.next
+            
+        else:
+            self.head = value
 
     """
         Inserir elemento no final da lista
     """
     def insert_end(value):
-
+        pass
     """
         remover um elemento da lista
     """
     def remove(value):
-
+        pass
     """
         buscar um elemento da lista
     """
     def search(value):
-
+        pass
     """
         imprimir os elementos na lista
     """
     def print_list():
-
+        pass
     """
         retornar o tamanho da lista
     """
     def size():
-
+        pass
     """
         verificar se a lista está vazia
     """
     def is_empty():
+        pass
+
+lista = LinkedList()
+print(lista.insert_beginning(5))
+print(lista.insert_beginning(3))
---
+++ pilhas.py
@@ -0,0 +1,13 @@
+"""
+para o primeiro elemento utilizo head
+para o ultimo elemento utilizo o tail
+
+lifo(last in firt out) e fifo(first in first out) 
+
+dentro da pilha temos 4 operações: 
+push
+pop
+peek(top)
+isEmpty
+"""
+
