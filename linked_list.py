0144f393e99a4cf0ada67885201063ae6bc35ddd
camarottoMD <matheus.domingues2@estudante.ifms.edu.br>
2026-03-12 17:18:27 -0300

    implementa método de remoção na lista encadeada com tratamento de exceções

--- linked_list.py
+++ linked_list.py
@@ -4,8 +4,11 @@
 class Node:
     def __init__(self, data=None):
         self.data = data
-        self.next = None #ISSO E O PONTEIRO 
+        self.next = None #ISSO É O VALOR DO PROXIMO NÓ 
 
+        """
+        PONTEIRO É ALGO QUE VAI APENAS GUIAR E APONTAR PARA UM NÓ
+        """
 
 class LinkedList:
     def __init__(self):
@@ -41,9 +44,24 @@ class LinkedList:
     """
         remover um elemento da lista
     """
-    def remove(self, value):
+    def remove(self, index):
+        if not self.head: #se trata de uma exceção, não faz sentido puxar um if normal
+            raise IndexError("Lista vazia") 
+        if index == 0:
+            self.head = self.head.next
+            return #finaliza o programa caso o indice seja 0
+        for i in range(index - 1):
+            if pointer.next: # isso vai evitar com que acesse um pointer == None, então verificamos sempre o proximo nó para não ter um erro
+                pointer = pointer.next #esse pointer vamos precisar atribuir pro nó anterior ao que vamos remover
+            else: 
+                raise IndexError("list index out of range")
+            
+        target = pointer.next #atribui o valor de none para o próximo nó, o nó que quero remover, pois a lista vai "esquecer" esse valor. 'Garbage Collection'
+        if target:
+            pointer.next = target.next #acessa o valor do nó seguinte ao que estou(index - 1), e atribui o valor do nó que esta após ao nó que quero remover
+        else:
+            raise IndexError("list index out of range")
         
-
     """
         buscar um elemento da lista
     """
